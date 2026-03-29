33e5ef061a9ad602ad26a3d60365394fb94ae78f
Metridat <abert2012@yandex.ru>
2026-03-29 21:33:09 +0500

    fix: update config to use postgres env variables from cluster

--- src/core/config.py
+++ src/core/config.py
@@ -2,9 +2,17 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    database_url: str
+    postgres_host: str
+    postgres_port: int
+    postgres_username: str
+    postgres_password: str
+    postgres_database_name: str
     events_provider_url: str
     events_provider_api_key: str
+
+    @property
+    def database_url(self) -> str:
+        return f"postgresql+asyncpg://{self.postgres_username}:{self.postgres_password}@{self.postgres_host}:{self.postgres_port}/{self.postgres_database_name}"
 
     model_config = {"env_file": ".env", "extra": "ignore"}
 
